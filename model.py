5ccc5e1dc2fceb23f45ea7aa25dad5aa5c5c4956
satojkovic <satojkovic@gmail.com>
2019-09-22 11:38:20 +0900

    Image caption model

---
+++ model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+from keras.models import Model
+from keras.layers import Dropout, Dense, Embedding, LSTM
+from keras.layers.merge import add
+from keras import Input
+
+class ImgCapModel:
+    def __init__(self, img_embed_dim=2048, word_embed_dim=200, vocab_size=1652, max_length=34):
+        img_embed = Input(shape=(img_embed_dim,))
+        hidden_img = Dropout(0.5)(img_embed)
+        hidden_img = Dense(256, activation='relu')(hidden_img)
+        word_seq = Input(shape=(max_length,))
+        word_embed = Embedding(vocab_size, word_embed_dim, mask_zero=True)(word_seq)
+        word_embed = Dropout(0.5)(word_embed)
+        hidden_word = LSTM(256)(word_embed)
+        decoder = add([hidden_img, hidden_word])
+        decoder = Dense(256, activation='relu')(decoder)
+        outputs = Dense(vocab_size, activation='softmax')(decoder)
+        self.model = Model(inputs=[img_embed, word_seq], outputs=outputs)
+
+    def summary(self):
+        self.model.summary()
+
+if __name__ == "__main__":
+    img_cap_model = ImgCapModel()
+    img_cap_model.summary()
